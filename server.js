34fcba9c4326fc8dd992566abd10ec607e3cb4d7
Dianav21 <dianavargas214@gmail.com>
2023-02-22 22:21:04 -0500

    edited homework

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import recipeRouter from "./routes/recipe.js"
-import userRouter from "./routes/user.js";
+// import userRouter from "./routes/user.js";
+import authRoutes from "./routes/authRoutes.js"
+import passport from "passport";
 
 export default async function createServer(){
     const app = express();
 
     app.use(express.json());
-    app.use("/user", userRouter())
+    // app.use("/user", userRouter())
     app.use("/recipe", recipeRouter())
 
+    app.use("/auth", authRoutes)
     return app;
 }
